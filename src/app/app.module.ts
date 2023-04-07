2e040a2b73d4ecf2fbc6a49986d46e42f149cd66
MaksymBalaniuk <108130207+MaksymBalaniuk@users.noreply.github.com>
2023-04-07 21:27:56 +0300

    CodeBlockPaginatorPipe added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CodeBlockPaginatorPipe } from './pipe/code-block-paginator.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CodeBlockPaginatorPipe
   ],
   imports: [
     BrowserModule,
