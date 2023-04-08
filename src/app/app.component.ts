3949a92f9cb004e8cb6e90447aabfbc0527bc2a0
MaksymBalaniuk <108130207+MaksymBalaniuk@users.noreply.github.com>
2023-04-08 17:07:05 +0300

    AppComponent update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {ModalService} from "./service/modal.service";
+import {ErrorService} from "./service/error.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'JavaCodeSet';
+
+  constructor(public modalService: ModalService, public errorService: ErrorService) { }
 }
