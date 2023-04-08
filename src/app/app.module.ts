1e7d54d2fd485f0e88a4196da298cc5b8395e485
MaksymBalaniuk <108130207+MaksymBalaniuk@users.noreply.github.com>
2023-04-08 14:14:52 +0300

    DateFormatterPipe added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CodeBlockPaginatorPipe } from './pipe/code-block-paginator.pipe';
+import { DateFormatterPipe } from './pipe/date-formatter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CodeBlockPaginatorPipe
+    CodeBlockPaginatorPipe,
+    DateFormatterPipe
   ],
   imports: [
     BrowserModule,
